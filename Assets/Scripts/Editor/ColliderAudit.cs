9c8b2ce71800acc55ed45280d9049de9750eeb5e
Muhammad Areeb <waybig125@gmail.com>
2026-05-15 17:46:51 +0500

    Backup state before environment overhaul

--- Assets/Scripts/Editor/ColliderAudit.cs
+++ Assets/Scripts/Editor/ColliderAudit.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TheAlchemistsCrypt.Editor
 {
@@ -13,45 +14,62 @@ namespace TheAlchemistsCrypt.Editor
         }
 
         private Vector2 scrollPos;
-        private List<GameObject> suspiciousObjects = new List<GameObject>();
+        private List<MeshCollider> meshColliders = new List<MeshCollider>();
 
         private void OnGUI()
         {
-            if (GUILayout.Button("Find Invisible Colliders"))
+            EditorGUILayout.HelpBox("This tool lists all MeshColliders in the scene. Use it to find and remove invisible walls or problematic collision meshes manually.", MessageType.Info);
+
+            if (GUILayout.Button("Find All Mesh Colliders", GUILayout.Height(30)))
             {
                 RunAudit();
             }
 
-            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
-            foreach (var obj in suspiciousObjects)
+            if (meshColliders.Count > 0)
             {
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.ObjectField(obj, typeof(GameObject), true);
-                if (GUILayout.Button("Select"))
+                EditorGUILayout.LabelField($"Found {meshColliders.Count} MeshColliders", EditorStyles.boldLabel);
+                
+                scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+                
+                for (int i = meshColliders.Count - 1; i >= 0; i--)
                 {
-                    Selection.activeGameObject = obj;
+                    var col = meshColliders[i];
+                    if (col == null) 
+                    {
+                        meshColliders.RemoveAt(i);
+                        continue;
+                    }
+
+                    EditorGUILayout.BeginHorizontal("box");
+                    
+                    EditorGUILayout.ObjectField(col.gameObject, typeof(GameObject), true, GUILayout.Width(200));
+                    
+                    if (GUILayout.Button("Select", GUILayout.Width(80)))
+                    {
+                        Selection.activeGameObject = col.gameObject;
+                        EditorGUIUtility.PingObject(col.gameObject);
+                    }
+                    
+                    GUI.backgroundColor = Color.red;
+                    if (GUILayout.Button("Delete", GUILayout.Width(80)))
+                    {
+                        Undo.DestroyObjectImmediate(col);
+                        meshColliders.RemoveAt(i);
+                    }
+                    GUI.backgroundColor = Color.white;
+                    
+                    EditorGUILayout.EndHorizontal();
                 }
-                EditorGUILayout.EndHorizontal();
+                
+                EditorGUILayout.EndScrollView();
             }
-            EditorGUILayout.EndScrollView();
         }
 
         private void RunAudit()
         {
-            suspiciousObjects.Clear();
-            Collider[] colliders = Object.FindObjectsByType<Collider>(FindObjectsSortMode.None);
-            foreach (var col in colliders)
-            {
-                if (!col.isTrigger)
-                {
-                    var renderer = col.GetComponent<Renderer>();
-                    if (renderer == null || !renderer.enabled)
-                    {
-                        suspiciousObjects.Add(col.gameObject);
-                    }
-                }
-            }
-            Debug.Log($"Found {suspiciousObjects.Count} suspicious colliders.");
+            meshColliders.Clear();
+            meshColliders = Object.FindObjectsByType<MeshCollider>(FindObjectsSortMode.None).ToList();
+            Debug.Log($"Found {meshColliders.Count} MeshColliders in the scene.");
         }
     }
 }
